b4c6843876853bd7ffb767ffcc11ff912c98440b
Luke Chambers <consolelogluke@gmail.com>
2023-03-18 22:44:48 -0400

    Add a minimal Paper plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     `java-library`
+    id("xyz.jpenilla.run-paper") version "2.0.1"
 }
 
 group = "sh.lpx"
 version = "1.0.0-SNAPSHOT"
 description = "A modern plugin manager for Paper."
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+}
 
 java {
     toolchain {
@@ -13,6 +23,14 @@ java {
 }
 
 tasks {
+    processResources {
+        val properties = mapOf("version" to project.version, "description" to project.description)
+        inputs.properties(properties)
+        filesMatching("paper-plugin.yml") {
+            expand(properties)
+        }
+    }
+
     withType<JavaCompile>().configureEach {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
@@ -25,5 +43,9 @@ tasks {
     @Suppress("UnstableApiUsage")
     withType<ProcessResources>().configureEach {
         filteringCharset = Charsets.UTF_8.name()
+    }
+
+    runServer {
+        minecraftVersion("1.19.4")
     }
 }
---
+++ src/main/java/sh/lpx/outlet/Outlet.java
@@ -0,0 +1,6 @@
+package sh.lpx.outlet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Outlet extends JavaPlugin {
+}
